4c9752a6c5615c102df84b94332da1b1df8882b3
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-14 17:21:03 +0300

    Add requirements.txt and .env

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h$$%sa7u-qk!-8u&c8jji@n8%6es15k2m4&&(48z(rb^%&r@r$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
