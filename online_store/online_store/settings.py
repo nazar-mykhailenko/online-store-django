7143a6d9a5c9447af950eb716741e10f850389a3
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-14 21:16:10 +0300

    Add base.html and styles

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -117,8 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIR = [
+    BASE_DIR / 'static_files'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
