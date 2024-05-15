eed5ad11e35582c83451f54c9106787657df9c60
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-15 22:56:24 +0300

    Add item filters

--- online_store/core/views.py
+++ online_store/core/views.py
@@ -2,14 +2,27 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
-from .forms import SignupForm
-from .models import Item
+from .forms import SignupForm, ItemFilterForm
+from .models import Item, Category
 
 # Create your views here.
 def index(request):
     items = Item.objects.filter()
+    if request.method == 'POST':
+        filter_form = ItemFilterForm(request.POST)
+        choises = [('', '')] + list(map(lambda c: (c.id, c.name), Category.objects.filter()))
+
+        filter_form.fields['category'].choices = choises
+        if filter_form.is_valid():
+            items = filter_form.filter_items(items)
+            print(filter_form.cleaned_data['category'])
+    else:
+        filter_form = ItemFilterForm()
+
+
     return render(request, "core/index.html",{
-        "items" : items
+        "items" : items,
+        'filter_form': filter_form
     })
 
 def signup(request):
