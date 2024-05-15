209fd97354139f697f874f899a3012d73435bb0d
Cubball <138378823+Cubball@users.noreply.github.com>
2024-05-15 22:01:31 +0300

    Add views and urls

--- online_store/cart/urls.py
+++ online_store/cart/urls.py
@@ -1,2 +1,11 @@
+from django.urls import path
+from . import views
+
+
 app_name = 'cart'
-urlpatterns = []
+urlpatterns = [
+    path('cart', views.cart, name='cart'),
+    path('cart-add/<int:product_id>', views.cart_add, name='cart-add'),
+    path('cart-remove/<int:product_id>', views.cart_remove, name='cart-remove'),
+    path('cart-remove-all/<int:product_id>', views.cart_remove_completely, name='cart-remove-all')
+]
