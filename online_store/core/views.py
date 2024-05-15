3611725cc6be100d259194ba2b0d98eafe6a4372
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-15 23:06:10 +0300

    Fix categories when method is not post

--- online_store/core/views.py
+++ online_store/core/views.py
@@ -8,16 +8,16 @@ from .models import Item, Category
 # Create your views here.
 def index(request):
     items = Item.objects.filter()
+    choises = [('', '')] + list(map(lambda c: (c.id, c.name), Category.objects.filter()))
     if request.method == 'POST':
         filter_form = ItemFilterForm(request.POST)
-        choises = [('', '')] + list(map(lambda c: (c.id, c.name), Category.objects.filter()))
 
         filter_form.fields['category'].choices = choises
         if filter_form.is_valid():
             items = filter_form.filter_items(items)
-            print(filter_form.cleaned_data['category'])
     else:
         filter_form = ItemFilterForm()
+        filter_form.fields['category'].choices = choises
 
 
     return render(request, "core/index.html",{
