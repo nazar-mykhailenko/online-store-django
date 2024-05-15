8ae8a154e7fad1aca59d2e27a7c9abde9fb980e4
Cubball <138378823+Cubball@users.noreply.github.com>
2024-05-15 21:43:54 +0300

    Add cart app

---
+++ online_store/cart/urls.py
@@ -0,0 +1,2 @@
+app_name = 'cart'
+urlpatterns = []
--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'cart'
 ]
 
 MIDDLEWARE = [
