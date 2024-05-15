1620c95325ae923c6bc0c852404ec65af1e439b5
Cubball <138378823+Cubball@users.noreply.github.com>
2024-05-15 21:46:36 +0300

    Add CartItem model

--- online_store/cart/models.py
+++ online_store/cart/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import Item
 
-# Create your models here.
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def get_total(self):
+        return self.quantity * self.product.price
+
+    class Meta:
+        unique_together = (('user', 'product'),)
