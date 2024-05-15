f0eed0cac7052e5dd6f31d115d1378c696ed7d82
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-15 18:02:48 +0300

    Add docker

---
+++ online_store/start.sh
@@ -0,0 +1,11 @@
+python manage.py migrate
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
+python manage.py runserver 0.0.0.0:8000
